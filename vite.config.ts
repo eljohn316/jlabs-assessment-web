d743a704a4b6794f937c384e508e02ddfdfcf574
El John <bongaeljohn07@gmail.com>
2025-07-02 11:18:45 +0800

    install and configure tanstack router, setup initial routes

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,17 @@ import { defineConfig } from 'vite';
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-  plugins: [tailwindcss(), react()],
+  plugins: [
+    tanstackRouter({ target: 'react', autoCodeSplitting: true }),
+    tailwindcss(),
+    react(),
+  ],
 });
