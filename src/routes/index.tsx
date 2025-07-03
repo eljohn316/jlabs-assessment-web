a2fe8bc4f9ceb09aa84aff05df723b0f7b410a01
El John <bongaeljohn07@gmail.com>
2025-07-03 19:18:56 +0800

    change nav color

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,15 +7,12 @@ export const Route = createFileRoute('/')({
 function RouteComponent() {
   return (
     <div className="mx-auto max-w-5xl px-4 py-10 sm:px-6">
-      <header className="flex items-center justify-between rounded-xl bg-blue-800 px-4 py-4 sm:px-6">
-        <p className="font-bold text-blue-50">Dashboard</p>
-        <div className="flex items-center gap-x-4">
-          <div className="size-8 rounded-full bg-blue-50" />
-          <button className="cursor-pointer text-sm font-medium text-blue-50 hover:underline">
-            Sign out
-          </button>
-        </div>
-      </header>
+      <nav className="flex items-center justify-between rounded-xl bg-gray-900 px-4 py-4 sm:px-6">
+        <p className="font-bold text-gray-50">Dashboard</p>
+        <button className="cursor-pointer text-sm font-medium text-gray-50 hover:underline">
+          Sign out
+        </button>
+      </nav>
       <div className="py-20 text-center">
         <p className="text-lg text-gray-700">
           Welcome <span className="font-bold text-blue-800">User</span>
